52335685b25cd2ae9516061d1d48d98f55feafe4
Tse Young <yangze0930@outlook.com>
2016-06-06 21:39:45 +0800

    version 1.0

---
+++ magicFilter/magicFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magicFilter
+//
+//  Created by tse on 16/6/6.
+//  Copyright © 2016年 tse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
